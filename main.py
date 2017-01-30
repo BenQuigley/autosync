5e15bf50e8571da6eb0f217679f0b444f320d77b
Ben Quigley <benquigleyconsulting@gmail.com>
2017-01-30 11:43:03 -0500

    added spacing, and implemented get_file function

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from pprint import PrettyPrinter
 from ids_hack import ids as missing_ids
 from tabulate import tabulate
 
-# Read the BoCo and Berklee rosters, create student and class records, 
-# and identify any students not registered in both systems.
-
 pp = PrettyPrinter(indent=4)
+
+# Read the home school and host school registration records, create student 
+# and class records, and identify any students not registered in both systems.
 
 class Institution():
     def __init__(self, name):
@@ -23,7 +23,7 @@ class Institution():
 
     def make_dict(self, location, student_id_column, course_section_column):
 
-        # Make a big dictionary of data from a file.
+        # Turn a CSV into a three-level dictionary containing the same data.
         # The dicts look like: data = {student_id: {class_1: {'header like Student Name': 'value like Joe Schmoe',
         #                                                     'header like Birth Date': 'value like 04/19/2000'}...}...}
 
@@ -48,17 +48,16 @@ class Institution():
                     # Name the third-level keys after the CSV's original headers.
 
                     data[p_key][course][headers[i]] = val
-        print('Read {} student records from {}.'.format(len(data), location))
+        print('Read {} student records from {}.\n'.format(len(data), location))
         return data
 
     def read_home_roster(self):
 
-        # Read the SIS output data from the students' home institution:
+        # Read the SIS output data from the students' home institution.
+        csv_file = get_file('IntegCrsOff_BCB2BCM_*.csv', target_dir='/home/iroh/cross-reg/')
+        data = self.make_dict(csv_file, student_id_column=9, course_section_column=6)
 
-        data = self.make_dict('/home/iroh/cross-reg/IntegCrsOff_BCB2BCM_2017-01-26.csv',
-                              student_id_column=9, course_section_column=6)
-
-        # Interpret the data by creating Student objects:
+        # Interpret the data by creating Student objects and adding them to institution.students.
 
         for student_id in data:
             courses = data[student_id]
@@ -66,6 +65,9 @@ class Institution():
                 record = courses[course]
                 if not student_id in self.students:
                     self.add_student(name=student_id, data=record)
+
+        # Add each class that the student is in to the student's home registrations list.
+
                 student = self.students[student_id]
                 record['active'] = (record['Add or Drop'] == 'Add')
                 names = {'eff date': 'Last Revision', 'grade': 'Final Grade', 'active': 'active'}
@@ -75,16 +77,18 @@ class Institution():
     def read_foreign_roster(self):
 
         # Read the SIS output data from the host institution:
-
-        data = self.make_dict('/home/iroh/Downloads/BoCo Integrated Offerings Registrations for A Given Term.csv',
-                              student_id_column=0, course_section_column=3)
+        csv_file = get_file('BoCo Integrated Offerings Registrations for A Given Term.csv', target_dir='/home/iroh/cross-reg')
+        data = self.make_dict(csv_file, student_id_column=0, course_section_column=3)
 
         # Some students without home institution IDs entered into the host system, either
         # because their data was entered incompletely or not at all or because they are not students
         # from this institution at all, will all get crammed into data[''].
-        # Delete data[''] for that reason and raise them in a few lines with try: data[student_id]; except KeyError: ...
+        # Delete data[''] for that reason and handle these instead later with: try: data[student_id]; except KeyError: ...
 
         del(data[''])
+
+        # For each student, find their registrations at the host school and add them to 
+        # the student's host registrations list.
 
         for student_id in self.students:
             student = self.students[student_id]
@@ -95,6 +99,11 @@ class Institution():
                     record['active'] = (record['Status- Current'][0] in ['A', 'N'])
                     names = {'eff date': 'Chg- Date', 'grade': 'Grade- Verified', 'active': 'active'}
                     student.register(mode='foreign', course_sec=course, course_data = record, names=names)
+
+        # If the student isn't on the host registrations list, and should be according to the home institution,
+        # then raise this error containing the information needed to add them to the host SIS.
+        # TODO: update this to print out in a nice table like the other errors.
+
             except KeyError:
                 if student.active:
                     pass
@@ -102,11 +111,15 @@ class Institution():
                     'Birth date: {}'.format(student.dob), 'Email: {}'.format(student.email), sep='\n')
                     print('home registrations:')
                     pp.pprint(student.registrations['home'])
+                    print()
 
 
 class Student():
 
     def __init__(self, name, data):
+        
+        # Save student data from the dictionary that came from the CSV.
+        
         self.name = name #home key
         self.real_name = data['Student']
         self.for_key = data['BCM_StudID']
@@ -123,7 +136,9 @@ class Student():
 
 
     def register(self, mode, course_sec, course_data, names):
-        # Save the column headers for the respective
+
+        # Save course data from the dictionary that came from the CVS.
+
         self.registrations[mode][course_sec] = {}
         for name in names:
             self.registrations[mode][course_sec][name] = course_data[names[name]]
@@ -160,9 +175,6 @@ class Student():
         return errors
 
 def main():
-
-    # Doing work
-
     boco = Institution('Boston Conservatory')
     boco.read_home_roster()
     boco.read_foreign_roster()
@@ -173,9 +185,8 @@ def main():
         if errors:
             for error in errors:
                 output.append(error)
-    #print(output)
     print(tabulate(sorted(output), headers=['Name', 'BoCo ID', 'Berklee ID', 'Class', 'Add / Drop', 'Update Date']))
-    print('Saved list:')
+    print('\nSaved list for Colleague:')
     for row in output:
         print(row[2])
 
